6f65eb3afed35018b74b01c2f6ce53b30b1be15c
danil <minakonn9shkoni@gmail.com>
2022-07-04 16:42:15 +0300

    correct message mongoose

--- src/db/models/message.js
+++ src/db/models/message.js
@@ -1,21 +1,46 @@
 const mongoose = require("mongoose");
 
-const messageSchema = new mongoose.Schema({
-body:{
-  type:String,
-  required:true,
-},
-author_id:{
-  type:mongoose.ObjectId,
-  required:true,
-  ref:"users",
-},
-to_user_id:{
-  type:mongoose.ObjectId,
-  required:true,
-  ref:"users",
-}
+const attachmentsSchema = new mongoose.Schema({
+  link: {
+    type: String,
+    required:true,
+  },
+  type:{
+    type:String,
+    enum:["photo","video","http","message","audio"],
+    required:true,
+  }
+});
 
+const messageSchema = new mongoose.Schema({
+  body: {
+    type: String,
+    required: true,
+  },
+  author_id: {
+    type: mongoose.ObjectId,
+    required: true,
+    ref: "users",
+  },
+  to_user_id: {
+    type: mongoose.ObjectId,
+    required: true,
+    ref: "users",
+  },
+  isRed: {
+    type: Boolean,
+    default: false,
+  },
+  isEdited: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+  },
+  attachments: {
+    type: [attachmentsSchema],
+  },
 });
 
 const messages = mongoose.model("messages", messageSchema);
