4e05e5be82ad1a1c2ffab512bac26ef0b12da025
danil <minakonn9shkoni@gmail.com>
2022-07-05 12:53:38 +0300

    Add some correction to service

--- src/services/message.service.js
+++ src/services/message.service.js
@@ -1,62 +1,121 @@
-const { ObjectId } = require("mongodb");
-const MessageModel = require("../db/models/message");
-const userModel = require("../db/models/user");
+// const { ObjectId } = require("mongodb");
+// const MessageModel = require("../db/models/message");
+// const userModel = require("../db/models/user");
+
+// module.exports.createMessage = async (data) => {
+//   const Message = MessageModel();
+//   const User = userModel();
+
+//   const author = await User.findOne({ _id: ObjectId(data.author_id) });
+//   if (!author) {
+//     throw new Error("404 Author not found");
+//   }
+
+//   const interlocutor = await User.findOne({ _id: ObjectId(data.to_user_id) });
+//   if (!interlocutor) {
+//     throw new Error("404 Interlocutor not found");
+//   }
+
+//   const result = await Message.create(data);
+//   // result.insertedId
+
+//   return { _id: result.insertedId, ...data };
+// };
+
+// module.exports.deletedMessageById = async (id) => {
+//   try {
+//     const Message = MessageModel();
+
+//     const result = await Message.deleteOne({ _id: ObjectId });
+
+//     return result;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// module.exports.findMessageById = async (id) => {
+//   try {
+//     const Message = MessageModel();
+
+//     const result = await Message.findOne({ _id: ObjectId(id) });
+
+//     return result;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// module.exports.findMessage = async (filter, page = 1, rows = 10) => {
+//   try {
+//     const Message = MessageModel();
+
+//     const result = await Message.find(filter)
+//       .skip((page - 1) * rows)
+//       .limit(rows)
+//       .toArray();
+
+//     return result;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+
+const { User, Message } = require("../db/");
 
 module.exports.createMessage = async (data) => {
-  const Message = MessageModel();
-  const User = userModel();
+  // check if author and user_to exists
+  const author = await User.findById(data.author_id).exec();
 
-  const author = await User.findOne({ _id: ObjectId(data.author_id) });
   if (!author) {
     throw new Error("404 Author not found");
   }
 
-  const interlocutor = await User.findOne({ _id: ObjectId(data.to_user_id) });
+  const interlocutor = await User.findById(data.to_user_id).exec();
   if (!interlocutor) {
     throw new Error("404 Interlocutor not found");
   }
 
-  const result = await Message.insertOne(data);
+  const result = await Message.create(data);
   // result.insertedId
 
-  return { _id: result.insertedId, ...data };
+  return result;
 };
 
-module.exports.deletedMessageById = async (id) => {
-  try {
-    const Message = MessageModel();
+module.exports.deleteMessageById = async (id) => {
+  const result = await Message.deleteOne({ _id: id });
 
-    const result = await Message.deleteOne({ _id: ObjectId });
+  // Message = findOne({ _id: id })
 
-    return result;
-  } catch (error) {
-    console.log(error);
-  }
+  return result.deletedCount > 0;
 };
 
 module.exports.findMessageById = async (id) => {
-  try {
-    const Message = MessageModel();
+  const result = await Message.findById(id).exec();
 
-    const result = await Message.findOne({ _id: ObjectId(id) });
-
-    return result;
-  } catch (error) {
-    console.log(error);
-  }
+  return result;
 };
 
-module.exports.findMessage = async (filter, page = 1, rows = 10) => {
-  try {
-    const Message = MessageModel();
+module.exports.findMessage = async (filter, page = 1, rows = 5) => {
+  const results = await Message.find(filter) // WHERE
+    .skip((page - 1) * rows) /// OFFSET
+    .limit(rows) // LIMIT
+    .exec();
 
-    const result = await Message.find(filter)
-      .skip((page - 1) * rows)
-      .limit(rows)
-      .toArray();
+  return results;
+};
 
-    return result;
-  } catch (error) {
-    console.log(error);
+module.exports.updateMessageById = async (id, data = {}) => {
+  // delete author_id and user_to fields
+
+  const updatedMessage = await Message.updateOne({ _id: id }, { $set: { ...data } });
+
+  if (updatedMessage.modifiedCount !== 1) {
+    throw new Error("404 Message not found");
   }
+
+  const foundMessage = await Message.findById(id).exec();
+
+  return foundMessage;
 };
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,68 +1,44 @@
 const { ObjectId } = require("mongodb");
-const userModel = require("../db/models/user");
 
 module.exports.createUser = async (data) => {
-  try {
-    const User = userModel();
+  const result = await User.create(data);
+  // result.insertedId
 
-    const result = await User.insertOne(data);
-    // result.insertedId
-
-    return result.insertedId;
-  } catch (error) {
-    console.log(error);
-  }
+  return result;
 };
 
 module.exports.deletedUserById = async (id) => {
-  try {
-    const User = userModel();
 
-    const result = await User.deleteOne({ _id: ObjectId });
-
-    return result;
-  } catch (error) {
-    console.log(error);
-  }
+    const result = await User.deleteOne({ _id: id });
 };
-
 
 module.exports.findUserById = async (id) => {
-  try {
-    const User = userModel();
-    
-    const result = await User.findOne({ _id: ObjectId(id) });
+
+    const result = await User.findOne({ _id: id });
 
     return result;
-  } catch (error) {
-    console.log(error);
-  }
+
 };
 
-    module.exports.findUsers = async (filter, page = 1, rows = 10) => {
-      try {
-        const User = userModel();
-    
-        const result = await User.find(filter)
-          .skip((page - 1) * rows)
-          .limit(rows)
-          .toArray();
-    
-        return result;
-      } catch (error) {
-        console.log(error);
-      }
-    };
+module.exports.findUsers = async (filter, page = 1, rows = 10) => {
 
-    // module.exports.updateUserById = async (id,data) => {
-    //   try {
-    //     const User = userModel();
-    //     const updatedUser =await User.updateOne({_id:ObjectId(id)},{$set:{...data}});
-        
-    //     const foundUser = await User.findOne({_id:ObjectId(id)});
+    const User = userModel();
 
-    //     return foundUser;
-    //   } catch (error) {
-    //     console.log(error);
-    //   }
-    // };
+    const result = await User.find(filter)
+      .skip((page - 1) * rows)
+      .limit(rows)
+
+    return result;
+};
+
+// module.exports.updateUserById = async (id, data = {}) => {
+//   const updatedUser = await User.updateOne({ _id: id }, { $set: { ...data } });
+
+//   if (updatedUser.modifiedCount !== 1) {
+//     throw new Error("404 User not found");
+//   }
+
+//   const foundUser = await User.findOne({ _id: id });
+
+//   return foundUser;
+// };
