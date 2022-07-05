786c5c9318d9e283e753ff4d75282959de68cb73
danil <minakonn9shkoni@gmail.com>
2022-07-05 12:59:30 +0300

    Add small corection

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,4 +1,4 @@
-const { ObjectId } = require("mongodb");
+const { User } = require("../db/");
 
 module.exports.createUser = async (data) => {
   const result = await User.create(data);
@@ -8,27 +8,23 @@ module.exports.createUser = async (data) => {
 };
 
 module.exports.deletedUserById = async (id) => {
-
-    const result = await User.deleteOne({ _id: id });
+  const result = await User.deleteOne({ _id: id });
 };
 
 module.exports.findUserById = async (id) => {
+  const result = await User.findOne({ _id: id });
 
-    const result = await User.findOne({ _id: id });
-
-    return result;
-
+  return result;
 };
 
 module.exports.findUsers = async (filter, page = 1, rows = 10) => {
+  const User = userModel();
 
-    const User = userModel();
+  const result = await User.find(filter)
+    .skip((page - 1) * rows)
+    .limit(rows);
 
-    const result = await User.find(filter)
-      .skip((page - 1) * rows)
-      .limit(rows)
-
-    return result;
+  return result;
 };
 
 // module.exports.updateUserById = async (id, data = {}) => {
